dfc6ca475cf244a9556360ad98af392084356787
maserabri <briannatran45@gmail.com>
2023-05-07 18:14:48 -0700

    updating

--- IntermediateJS/hofTimersClosuresExercises.js
+++ IntermediateJS/hofTimersClosuresExercises.js
@@ -126,3 +126,23 @@ function find(arr, fn){
 
 find([8,11,4,27], function(val){return val >= 10}); // 11
 find([8,11,4,27], function(val){return val === 5}); // undefined
+
+/*Write a function called findIndex. It should take in an array and a callback and return the index of first value found in the array 
+that matches the condition.*/
+
+//iterate through arr using for...loop
+    //when callback fn is invoked accepting arr val and returns true,
+        //return the index
+
+function findIndex(arr, fn){
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i])){
+            return i;
+        }
+    }
+}
+
+// returns 1 (index of the first value greater than or equal to 10)
+findIndex([8,11,4,27], function(val){return val >= 10});
+
+findIndex([8,11,4,27], function(val){return val === 7}); // undefined
