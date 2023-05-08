f432b8bf3c7216a893f0e224dddcd8246cca69cf
maserabri <briannatran45@gmail.com>
2023-05-07 18:26:21 -0700

    updating exercises

--- IntermediateJS/hofTimersClosuresExercises.js
+++ IntermediateJS/hofTimersClosuresExercises.js
@@ -146,3 +146,26 @@ function findIndex(arr, fn){
 findIndex([8,11,4,27], function(val){return val >= 10});
 
 findIndex([8,11,4,27], function(val){return val === 7}); // undefined
+
+/*Write a function called specialMultiply which accepts two parameters. If the function is passed both parameters, it should 
+return the product of the two. If the function is only passed one parameter – it should return a function which can later be 
+passed another parameter to return the product. You will have to use closure and arguments to solve this.*/
+
+//if arguments length is 1,
+    //return anonymous function that accepts b as a parameter
+        //return product of a and b
+//else,
+    //return product of a and b
+
+function specialMultiply(a, b){
+    if(arguments.length === 1){
+        return function(b){
+            return a * b;
+        }
+    }
+    return a * b;
+}
+
+specialMultiply(3,4); // 12
+specialMultiply(3)(4); // 12
+specialMultiply(3); // returns a function 
