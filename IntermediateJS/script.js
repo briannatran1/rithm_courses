22b6d1f00b8cbccd66083f1c001b44ff77b602c1
maserabri <briannatran45@gmail.com>
2023-05-12 11:58:54 -0700

    started p2 of script

--- IntermediateJS/script.js
+++ IntermediateJS/script.js
@@ -24,6 +24,9 @@ window.onload = function(){
 
     /*Create a racing game with the two cars. When the race button is pressed, the two cars should move across the screen until one 
     of them is at the end of the screen. When one of the blocks reaches the end – you should alert “winner!”*/
+    let car1 = document.querySelector('.car1');
+    let car2 = document.querySelector('.car2');
+
     
 }
 
