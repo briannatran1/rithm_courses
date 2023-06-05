32e0bcfea3d36bd7cbed3f3ac020faf768bc7b00
maserabri <briannatran45@gmail.com>
2023-06-05 11:08:31 -0700

    added jQuery methods

---
+++ intermediate-js-p2/common-jQuery-methods/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('article').html(); // innerHTML
+    $('article').text(); //innerText
+    $('input').val(); // value
+}); //add strings to these methods to set instead of get
+
+
+
