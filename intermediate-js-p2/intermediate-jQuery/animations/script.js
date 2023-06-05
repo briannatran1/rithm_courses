a800466893f76fcad73a605729ed64269e3b4af7
maserabri <briannatran45@gmail.com>
2023-06-05 12:28:40 -0700

    added animate function

--- intermediate-js-p2/intermediate-jQuery/animations/script.js
+++ intermediate-js-p2/intermediate-jQuery/animations/script.js
@@ -14,3 +14,16 @@ $(document).ready(function(){
     });
 });
 
+// using animate function
+$(document).ready(function(){
+    $('.items').click(function(){
+        $('li').animate({
+            // what properties to manipulate
+            opacity: 0.5,
+            marginLeft: '+=100',
+        }, 5000, function(){
+            //this code will be executed when the animation is complete
+            $('p').css('font-size', '+=5');
+        });
+    });
+});
