6f57740650a7eb92a32f02b8d4c46ef3fc74bbe0
maserabri <briannatran45@gmail.com>
2023-06-05 11:16:21 -0700

    added element styles

--- intermediate-js-p2/common-jQuery-methods/script.js
+++ intermediate-js-p2/common-jQuery-methods/script.js
@@ -7,6 +7,14 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('article').addClass('hidden'); // add a class
     $('article').removeClass('hidden'); // remove a class
-    $('article').toggleClass('hidden'); //toggle the class (if on -> off, vice versa)
+    $('article').toggleClass('hidden'); // toggle the class (if on -> off, vice versa)
+});
+
+// accessing element styles
+$(document).ready(function(){
+    $('article').css('background-color', 'red');
+    $('article').css('background-color'); // red
+    $('article').attr('style', 'display:flex');
+    $('article').data('id', '1');
 });
 
