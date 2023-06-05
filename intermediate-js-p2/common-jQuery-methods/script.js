4583d568cb635f4a084b43bc0d8996ac290ecf1a
maserabri <briannatran45@gmail.com>
2023-06-05 11:10:35 -0700

    added class methods

--- intermediate-js-p2/common-jQuery-methods/script.js
+++ intermediate-js-p2/common-jQuery-methods/script.js
@@ -4,5 +4,9 @@ $(document).ready(function(){
     $('input').val(); // value
 }); //add strings to these methods to set instead of get
 
-
+$(document).ready(function(){
+    $('article').addClass('hidden'); // add a class
+    $('article').removeClass('hidden'); // remove a class
+    $('article').toggleClass('hidden'); //toggle the class (if on -> off, vice versa)
+});
 
