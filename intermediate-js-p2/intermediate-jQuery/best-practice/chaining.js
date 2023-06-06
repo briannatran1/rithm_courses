94dc1399f1d278139c5bd953d6e764eb3ede6367
maserabri <briannatran45@gmail.com>
2023-06-06 14:33:31 -0700

    added best practices

---
+++ intermediate-js-p2/intermediate-jQuery/best-practice/chaining.js
@@ -0,0 +1,10 @@
+$("div").first().parent().find(".projects").css("color","red").fadeOut(200);
+
+// equals
+
+$('div')
+    .first()
+    .parent()
+    .find('.projects')
+    .css('color', 'red')
+    .fadeOut(200);
