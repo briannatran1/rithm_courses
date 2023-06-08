9f4e7083fefafbaa32b69dc41c85fe1db9a9fb18
maserabri <briannatran45@gmail.com>
2023-06-08 15:03:10 -0700

    added addMovie and clearForm functions

--- intermediate-js-p2/intermediate-jQuery/exercises/script.js
+++ intermediate-js-p2/intermediate-jQuery/exercises/script.js
@@ -1,3 +1,29 @@
 $(function(){
+    let movies = [];
+
+    // clearing form
+    function clearForm(){
+        $('#title').val('');
+        $('#rating').val('');
+    }
+
+    // rendering table
+    function renderTable(){
+        
+    }
     
+    // adding movies
+    function addMovie(){
+        let title = $('#title').val();
+        let rating = $('#rating').val();
+        if(rating && title){
+            let movie = {
+                title: title,
+                rating: rating
+            };
+            movies.push(movie);
+            renderTable();
+            clearForm();
+        }
+    }
 });
