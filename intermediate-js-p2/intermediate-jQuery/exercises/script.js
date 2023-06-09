eca5fc9c7723a617661955d2d83bfd12733ac8f5
maserabri <briannatran45@gmail.com>
2023-06-09 11:44:05 -0700

    changed script

--- intermediate-js-p2/intermediate-jQuery/exercises/script.js
+++ intermediate-js-p2/intermediate-jQuery/exercises/script.js
@@ -1,72 +1,26 @@
-$(function(){
-    let movies = [];
+$(document).ready(function() {
+    // Submit form to add an item to the list
+    $('#list-form').submit(function(event) {
+        event.preventDefault();
 
-    // clearing form
-    function clearForm(){
-        $('#title').val('');
-        $('#rating').val('');
-    }
-
-    // rendering table
-    function renderTable(){
-        $('#movieTable tbody').empty();
-        movies.map(function(movie, index){
-            let row = '<tr>' + 
-                        '<td>' + movie.title + '</td>' +
-                        '<td>' + movie.rating + '</td>' +
-                        '<td><button class="btn btn-danger btn-sm delete" data-index="' + index + '">Delete</button></td>' +
-                      '</tr>'
-            $('#movieTable tbody').append(row);
-        });
-    }
-    
-    // adding movies
-    function addMovie(){
         let title = $('#title').val();
         let rating = $('#rating').val();
-        if(rating && title){
-            let movie = {
-                title: title,
-                rating: rating
-            };
-            movies.push(movie);
-            renderTable();
-            clearForm();
-        }
-    }
 
-    // delete movie
-    function deleteMovie(index){
-        movies.splice(index, 1);
-        renderTable();
-    }
+        // Add row to the table
+        let row = $('<tr>');
+        row.append($('<td>').text(title));
+        row.append($('<td>').text(rating));
+        row.append($('<td>').html('<button class="delete-btn btn btn-danger">Delete</button>'));
 
-    // sorting movies
-    function sortMovies(sortBy){
-        movies.sort((a, b) => {
-            if(sortBy === 'title'){
-                return a.title.localeCompare(b.title);
-            }   
-            else if(sortBy === 'rating'){
-                return b.rating - a.rating;
-            }
-        });
-        renderTable();
-    }
+        $('#list-table tbody').append(row);
 
-    // event listeners
-    $('#addButton').click(addMovie);
-
-    $(document).on('click', '.delete', function(){
-        let index = $(this).data('index');
-        deleteMovie(index);
+        // Clear form inputs
+        $('#title').val('');
+        $('#rating').val('');
     });
 
-    $('#sortTitle').on('click', function(){
-        sortMovies('title');
+    // Delete row from the table
+    $(document).on('click', '.delete-btn', function() {
+        $(this).closest('tr').remove();
     });
-
-    $('#sortRating').on('click', function(){
-        sortMovies('rating');
-    })
-});
+  });
