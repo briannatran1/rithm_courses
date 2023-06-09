f9373de6264e3dd4c503215f5077d0bd78770b36
maserabri <briannatran45@gmail.com>
2023-06-09 11:26:25 -0700

    updated script

--- intermediate-js-p2/intermediate-jQuery/exercises/script.js
+++ intermediate-js-p2/intermediate-jQuery/exercises/script.js
@@ -9,7 +9,15 @@ $(function(){
 
     // rendering table
     function renderTable(){
-        
+        $('#movieTable tbody').empty();
+        movies.map(function(movie, index){
+            let row = '<tr>' + 
+                        '<td>' + movie.title + '</td>' +
+                        '<td>' + movie.rating + '</td>' +
+                        '<td><button class="btn btn-danger btn-sm delete" data-index="' + index + '">Delete</button></td>' +
+                      '</tr>'
+            $('#movieTable tbody').append(row);
+        });
     }
     
     // adding movies
@@ -26,4 +34,39 @@ $(function(){
             clearForm();
         }
     }
+
+    // delete movie
+    function deleteMovie(index){
+        movies.splice(index, 1);
+        renderTable();
+    }
+
+    // sorting movies
+    function sortMovies(sortBy){
+        movies.sort((a, b) => {
+            if(sortBy === 'title'){
+                return a.title.localeCompare(b.title);
+            }   
+            else if(sortBy === 'rating'){
+                return b.rating - a.rating;
+            }
+        });
+        renderTable();
+    }
+
+    // event listeners
+    $('#addButton').click(addMovie);
+
+    $(document).on('click', '.delete', function(){
+        let index = $(this).data('index');
+        deleteMovie(index);
+    });
+
+    $('#sortTitle').on('click', function(){
+        sortMovies('title');
+    });
+
+    $('#sortRating').on('click', function(){
+        sortMovies('rating');
+    })
 });
